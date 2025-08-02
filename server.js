846bf91b1b2310888ca3a55b6352a48c70000220
lolodev <horaciodev1706@gmail.com>
2025-08-02 18:46:43 -0300

    Serve stringified JSON

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 import http from 'node:http';
+import { getDataFromDB } from './database/db.js';
 
 const PORT = 8000;
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
+    const destinations = await getDataFromDB();
+
+    /*
+    Challenge:
+      1. Store our data in a const ‘destinations’.
+      2. When a GET request is received to the ‘/api' endpoint, send our JSON stringified data.
+        Think: What changes will you need to make to get this to work?
+    */
 
     if (req.url === '/api' && req.method === 'GET') {
-        res.end('Hello from the server!');
+        res.end(JSON.stringify(destinations));
     }
 });
 
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+server.listen(PORT, () => console.log(`Connected on port: ${PORT}`));
