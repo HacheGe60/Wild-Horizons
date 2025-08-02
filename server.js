a82aa9a29726fd781d2dd8ca6a44a859e1ed71fc
lolodev <horaciodev1706@gmail.com>
2025-08-02 17:49:55 -0300

    The package.json file

--- server.js
+++ server.js
@@ -1,13 +1,1 @@
-import http from 'node:http';
-
-const PORT = 8000;
-
-
-const server = http.createServer((req, res) => {
-    if (req.url === '/api') {
-        res.end('This is from the server');
-    }
-
-});
-
-server.listen(PORT, () => console.log(`Connected on port: ${PORT}`));
+console.log('We have a package.json!!!');
